b59d49a862e55866f1ac672b2b2113958f914057
Ralex91 <95540504+Ralex91@users.noreply.github.com>
2025-01-09 11:33:26 +0100

    feature(products): add endpoint for get all products

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,15 @@
 import { Router } from "express"
+import db from "../lib/db.js"
 
 const router = Router()
 
 router.get("/", async (_, res) => {
-  res.json({ message: "Not implemented" })
+  try {
+    const [results] = await db.query("SELECT * FROM Products")
+    res.json(results)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
 })
 
 export default router
