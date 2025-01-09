b42125fe1d15911330f456a42666db4b735ecf79
Ralex91 <95540504+Ralex91@users.noreply.github.com>
2025-01-09 11:40:30 +0100

    feature(database): add endpoint for get a product with id

--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,24 @@ const router = Router()
 router.get("/", async (_, res) => {
   try {
     const [results] = await db.query("SELECT * FROM Products")
+
     res.json(results)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+router.get("/:id", async (req, res) => {
+  try {
+    const [results] = await db.query(
+      `SELECT * FROM Products WHERE product_id = ${req.params.id}`
+    )
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: "Product not found" })
+    }
+
+    res.json(results[0])
   } catch (error) {
     res.status(500).json({ error: error.message })
   }
